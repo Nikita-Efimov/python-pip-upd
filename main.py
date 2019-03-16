9bcb01b30994338eb860dc1dfd0e31c56322592f
nikita-efimov <nikikot@list.ru>
2019-03-16 14:41:33 +0300

    added complted msg

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
+import subprocess
 
-pip_name = str(input('input your pip name: '))
+pip_name = str(input('Enter your pip name: '))
 
 print(pip_name)
 
@@ -10,4 +11,8 @@ pckg_list = stream.read()
 
 for line in pckg_list.split('\n'):
     pckg = line.split('==')[0]
+    if pckg is '':
+        break
     os.system(' '.join([pip_name, 'install', '-U', pckg]))
+
+print('Completed')

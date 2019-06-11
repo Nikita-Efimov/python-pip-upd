48433a065c0afe58c9bbe0050283f31302260a60
nikita-efimov <nikikot@list.ru>
2019-06-11 17:04:05 +0300

    added pip update

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ stream = os.popen(' '.join([pip_name, 'list', '--format', 'freeze']))
 
 pckg_list = stream.read()
 
+os.system(' '.join([pip_name, 'install', '--upgrade', 'pip']))
+
 for line in pckg_list.split('\n'):
     pckg = line.split('==')[0]
     if pckg is '':
